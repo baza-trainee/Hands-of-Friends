c6149e1b338901ff020ffa5e037bf9c4a15f929d
dav1dashka <dujadavyd17@gmail.com>
2024-04-12 22:38:27 +0300

    refactor: change path to backend

--- src/app/helpers/consts.js
+++ src/app/helpers/consts.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://hands-of-friends-backend.onrender.com/api/";
+export const BASE_URL = "https://backend.rd.org.ua/api/";
 export const ULCLASS = "list-disc px-5 py-6";
 export const OLCLASS = "list-decimal px-5 py-6";
 export const phoneRegExp =
