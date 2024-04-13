8d48bdbe14dfb1422f01a4ce9b7a7b4156e4db23
dav1dashka <dujadavyd17@gmail.com>
2024-04-13 14:50:49 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["backend.rd.org.ua"],
+    domains: ['backend.rd.org.ua'],
+    deviceSizes: [640, 768, 1024, 1280, 1600, 3840],  // Include the requested size
+    imageSizes: [16, 32, 48, 64, 96, 256, 512],
   },
   webpack: (config) => {
     config.module.rules.push({
